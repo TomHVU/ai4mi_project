9dc7e80c9e708337e7d1559bd85d60a10d9bdcbe
Tom de Haas <t.de.haas2@student.vu.nl>
2025-09-17 17:11:56 +0200

    Added processing of all patient files

--- affine.py
+++ affine.py
@@ -1,5 +1,8 @@
 import pickle
 import random
+import os
+import sys
+from pathlib import Path
 import argparse
 import warnings
 from pathlib import Path
@@ -34,7 +37,6 @@ R2 = np.array([
 ], dtype=float)
 
 # T3 = inverse of T1
-#TODO: really?
 T3 = np.linalg.inv(T1)
 
 # T4
@@ -45,9 +47,12 @@ T4 = np.array([
     [0, 0, 0, 1]
 ], dtype=float)
 
+# Put matrices into one affine matrix
+combined_affine = T1 @ R2 @ T3 @ T4 
+
 # Patient 27 has both good and bad
 # TODO: import all patient images.
-def import_image(dest_path: Path, source_path: Path) -> nibabel.Nifti1Image:
+def import_image(source_path: Path) -> nibabel.Nifti1Image:
     # File location
     gt_path = source_path 
     assert gt_path.exists()
@@ -62,8 +67,9 @@ def import_image(dest_path: Path, source_path: Path) -> nibabel.Nifti1Image:
 
     return gt_nifti
 
-def detamper(image, matrix, reverse=True):
-    # Reverse the affine transformation
+def detamper(image, matrix, reverse=True) -> np.array:
+    # Reverse the affine transformation using the inverse
+    # Could be used as forward as well
 
     # If we want to reverse the transformation, use inverse
     if reverse:
@@ -75,89 +81,58 @@ def detamper(image, matrix, reverse=True):
     # Return image
     return detampered_img
 
-def main(args: argparse.Namespace):
-    #TODO: use single path, we put the file back anyways
-    src_path: Path = Path(args.source_dir)
-    dest_path: Path = Path(args.dest_dir)
+def main(src_path):
+    step_by_step = args.step_by_step
 
-    gt_img = import_image(dest_path, src_path / "GT.nii.gz")
+    # Extract nifti file and save affine and header
+    gt_img = import_image(src_path / "GT.nii.gz")
+    gt_header = gt_img.header
+    gt_affine = gt_img.affine
 
-    gt_header = gt_nifti.header
-    print(gt_header)
-    gt_img = gt_nifti.get_fdata().astype(np.int16)
-    affine = gt_nifti.affine
-    
+    # Get fdata to np.array(int16)
+    gt_img = gt_img.get_fdata().astype(np.int16)
+
     # Isolate the heart channel
     # The heart is channel 2
     gt_heart = np.where(gt_img == 2, gt_img, 0)
 
-    # Remove the incorrect channel
-    #TODO: convert to np image
+    # Remove the tampered heart
     gt_img[gt_img == 2] = 0
 
-    # Perform reverse transformation
-    # TODO: optimize performance
+    # To see separate matrix steps play out.
+    if step_by_step:
+        gt_heart_T4 = detamper(gt_heart, T4)
+        gt_heart_T3 = detamper(gt_heart_T4, T3)
+        gt_heart_R2 = detamper(gt_heart_T3, R2)
+        gt_heart_T1 = detamper(gt_heart_R2, T1)
 
-    combined_affine = T1 @ R2 @ T3 @ T4 
-
-    print("Performing Reverse transformations")
-    gt_heart_T4 = detamper(gt_heart, T4)
-    gt_heart_T3 = detamper(gt_heart_T4, T3)
-    gt_heart_R2 = detamper(gt_heart_T3, R2)
-    gt_heart_T1 = detamper(gt_heart_R2, T1)
-
+    # Reverse the affine transformation 
     gt_heart_combined = detamper(gt_heart, combined_affine)
-    print("Transformations finished")
 
     # Put back the heart
-    gt_img[gt_heart_T1 == 2] = 2
-
-    # For testing purposes
-    if patient_number == 27:
-
-        # Import patient 27
-        gt_img_27, patient_number, affine = import_image(dest_path, src_path / "GT2.nii.gz")
-
-        print(skimage.metrics.normalized_mutual_information(gt_img, gt_img_27, bins=100))
-        print(skimage.metrics.normalized_mutual_information(gt_img_27, gt_img_27, bins=100))
-
-        # TODO: check if similar
-        # assert np.array_equal(gt_img_27, gt_img)
-       
-    # Save image
-    gt_img = nibabel.Nifti1Image(gt_img, affine)
-    nibabel.save(gt_img, src_path / 'GT_detampered.nii.gz')
-
-    # Save image
-    gt_img = nibabel.Nifti1Image(gt_heart_T4, affine)
-    nibabel.save(gt_img, src_path / 'GT_T4.nii.gz')
-
-    # Save image
-    gt_img = nibabel.Nifti1Image(gt_heart_T3, affine)
-    nibabel.save(gt_img, src_path / 'GT_T3.nii.gz')
-    
-    # Save image  
-    gt_img = nibabel.Nifti1Image(gt_heart_R2, affine)
-    nibabel.save(gt_img, src_path / 'GT_R2.nii.gz')
+    gt_img[gt_heart_combined == 2] = 2
 
      # Save image  
-    gt_img = nibabel.Nifti1Image(gt_heart_combined, affine)
-    nibabel.save(gt_img, src_path / 'GT_combined.nii.gz')
+    gt_img = nibabel.Nifti1Image(gt_heart_combined, gt_affine, header=gt_header)
+    nibabel.save(gt_img, src_path / 'GT_fixed.nii.gz')
     
 def get_args() -> argparse.Namespace:
+
     parser = argparse.ArgumentParser(description = "Slicing parameters")
 
-    parser.add_argument('--source_dir', type=str, default="banana")
-    parser.add_argument('--dest_dir', type=str, default="banna")
+    parser.add_argument("--source_dir", type=str, default="data")
     parser.add_argument("--seed", type=int, default=42, help="Random seed")
+    parser.add_argument("--step_by_step", type=bool, default=False)
 
     args = parser.parse_args()
     random.seed(args.seed)
 
-    if args.dest_dir is None:
-        args.dest_dir = args.source_dir
-
     return args
 
 if __name__ == "__main__":
-    main(get_args())
+
+    args = get_args()
+    dir = Path(args.source_dir)
+    
+    for patient_file in dir.iterdir():
+        main(patient_file)
