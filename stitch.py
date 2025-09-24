a8f6b7533734d25c6494ea33530cf5a90304caa2
Tom de Haas <t.de.haas2@student.vu.nl>
2025-09-24 17:08:29 +0200

    Edited stitch.py to work in theory

--- stitch.py
+++ stitch.py
@@ -16,15 +16,27 @@ def save_3d_image(image_3d, path) -> nibabel.Niftyimage:
     """
     pass
     
+def main(args: argparse.Namespace):
+    data_path: Path = Path(args.source_dir)
+    dest_path: Path = Path(args.dest_dir)
+    if not data_path.exists():
+        data_path.mkdir(parents=True, exist_ok=True)
+    if not dest_path.exists():
+        dest_path.mkdir(parents=True, exist_ok=True)
+
+    assert data_path.exists()
+    assert dest_path.exists()
+
 def get_args() -> argparse.Namespace:
 
     parser = argparse.ArgumentParser(description = "Slicing parameters")
 
-    parser.add_argument("--data_folder", type=str, default="data")
-    parser.add_argument("--dest_folder", type=int, default=42, help="Random seed")
-    parser.add_argument("--num_classes", type=bool, default=False)
-    parser.add_argument("--grp_regex")
-    parser.add_argument("source_scan_pattern")
+    parser.add_argument("--data_folder", type=str, required=True)
+    parser.add_argument("--dest_folder", type=str, required=True)
+    parser.add_argument("--num_classes", type=int, default=255)
+    parser.add_argument("--grp_regex", type=str, default="(Patient_\d\d)_\d\d\d\d")
+    parser.add_argument("--source_scan_pattern", type=str, default="data/segthor_train/train/{id_}/GT.nii.gz")
+    parser.add_argument("--seed", type=int, default=42, help="Random Seed")
 
     args = parser.parse_args()
     random.seed(args.seed)
@@ -32,5 +44,4 @@ def get_args() -> argparse.Namespace:
     return args
 
 if __name__ == "__main__":
-    args = get_args()
-    dir = Path(args.source_dir)
+    main(get_args)
