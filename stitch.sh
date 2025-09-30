d2f67910c37ce2efa5993bcb935b6f0b6db2828b
Tom <t.de.haas2@student.vu.nl>
2025-09-30 14:30:23 +0200

    Added minor change to stitch bash script

--- stitch.sh
+++ stitch.sh
@@ -1,1 +1,4 @@
-python stitch.py --data_folder /home/scur0607/ai4mi_project/data/SEGTHOR_CLEAN/train/gt --dest_folder /home/scur0607/ai4mi_project/data/SEGTHOR_STITCHED/train/gt 
+conda activate ai4mi
+
+# python stitch.py --data_folder /home/scur0607/ai4mi_project/data/SEGTHOR_CLEAN/train/gt --dest_folder /home/scur0607/ai4mi_project/data/SEGTHOR_STITCHED/train/gt 
+python stitch.py --data_folder /home/scur0607/ai4mi_project/data/SEGTHOR_CLEAN/val/gt --dest_folder /home/scur0607/ai4mi_project/data/SEGTHOR_STITCHED/val/gt 
